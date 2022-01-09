dce2e415b1f34b730735e533851ebc97d0cd4482
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2022-01-09 10:36:45 +0600

    update function

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ const phoneQuantity = document.getElementById("phoneQuantity");
 const phonePrice = document.getElementById("phonePrice");
 
 plusBtnPhone.addEventListener("click", () => {
+  handleProductChange(true);
+});
+
+minusBtnPhone.addEventListener("click", () => {
+  handleProductChange(false);
+});
+
+function handleProductChange(isIncrease) {
   const phoneCount = parseInt(phoneQuantity.value);
-  const phoneNewCount = phoneCount + 1;
+  let phoneNewCount;
+  if (isIncrease == true) {
+    phoneNewCount = phoneCount + 1;
+  }
+  if (isIncrease == false && phoneCount > 0) {
+    phoneNewCount = phoneCount - 1;
+  }
   phoneQuantity.value = phoneNewCount;
   const phoneTotal = phoneNewCount * 1219;
   phonePrice.innerText = phoneTotal;
-});
+}
