5e5b363ef2c9e60832ef02bcaf7a52c1311c49ba
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2022-02-05 09:45:48 +0600

    update code

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const plusBtnPhone = document.getElementById("plusBtnPhone");
 const minusBtnPhone = document.getElementById("minusBtnPhone");
 const phoneQuantity = document.getElementById("phoneQuantity");
 const phonePrice = document.getElementById("phonePrice");
+const plusBtnCase = document.getElementById("plusBtnCase");
+const minusBtnCase = document.getElementById("minusBtnCase");
+const caseQuantity = document.getElementById("caseQuantity");
+const casePrice = document.getElementById("casePrice");
+const subtotal = document.getElementById("subtotal");
 
 plusBtnPhone.addEventListener("click", () => {
   handleProductChange(true);
@@ -11,9 +16,17 @@ minusBtnPhone.addEventListener("click", () => {
   handleProductChange(false);
 });
 
+plusBtnCase.addEventListener("click", () => {
+  handleCaseChange(true);
+});
+
+minusBtnCase.addEventListener("click", () => {
+  handleCaseChange(false);
+});
+
 function handleProductChange(isIncrease) {
   const phoneCount = parseInt(phoneQuantity.value);
-  let phoneNewCount;
+  let phoneNewCount = 0;
   if (isIncrease == true) {
     phoneNewCount = phoneCount + 1;
   }
@@ -23,4 +36,17 @@ function handleProductChange(isIncrease) {
   phoneQuantity.value = phoneNewCount;
   const phoneTotal = phoneNewCount * 1219;
   phonePrice.innerText = phoneTotal;
+}
+
+function handleCaseChange(isIncrease) {
+  const caseCount = parseInt(caseQuantity.value);
+  let caseNewCount = 0;
+  if (isIncrease == true) {
+    caseNewCount = caseCount + 1;
+  } else if (isIncrease == false && caseCount > 0) {
+    caseNewCount = caseCount - 1;
+  }
+  caseQuantity.value = caseNewCount;
+  const caseTotal = caseNewCount * 59;
+  casePrice.innerText = caseTotal;
 }
